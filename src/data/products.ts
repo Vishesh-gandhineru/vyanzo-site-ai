0c2fa385a0d8af4b2fa6d494ee1981a983b7cc46
Vishesh Jhadi <visheshjhadi29@gmail.com>
2026-03-10 14:45:20 +0530

    update some translations

--- src/data/products.ts
+++ src/data/products.ts
@@ -180,7 +180,7 @@ async function getStaticHydraulicCover(locale: string): Promise<Product | null> 
       productDetails: [],
       image: subImages.length > 0 ? subImages[0] : "",
       subImages,
-      category: locale === 'fr' ? 'Regards Hydrauliques' : locale === 'nl' ? 'Hydraulische deksels' : locale === 'de' ? 'Hydraulische Abdeckungen' : 'Hydraulic Covers',
+      category: locale === 'fr' ? 'Regards de chaussée' : locale === 'nl' ? 'wegenisdeksels' : raw.product_category,
       subCategory: raw.sub_category,
       certificationType: raw.certification_type,
       location: null,
